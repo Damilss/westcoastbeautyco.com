6004daa5c4868dbc7132c0699bcafe98fbec73e4
Emilio Scott <milii76@outlook.com>
2026-04-07 01:08:38 -0700

    throw in youtube embed ;)

---
+++ app/bottlerock/page.tsx
@@ -0,0 +1,7 @@
+<iframe width="560" 
+height="315" 
+src="https://www.youtube-nocookie.com/embed/lu7ZeNRr0TI?si=zGOPUoZwEj6H5n2O"
+ title="YouTube video player" frameborder="0" 
+ allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+ referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>   
+ </iframe>
